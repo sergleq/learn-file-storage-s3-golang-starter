cedd6bb892903bab41a4e724d812f33b35f1a30c
Sergleq <ma4mal@gmail.com>
2025-08-10 23:54:27 +0000

    upload thumbnail handler implement

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -9,6 +10,27 @@ import (
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
+	maxMemory := 10 << 20
+	if err := r.ParseMultipartForm(int64(maxMemory)); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "parsing is fail", err)
+		return
+	}
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+
+	byteImage, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to read form file", err)
+		return
+	}
+
 	videoIDString := r.PathValue("videoID")
 	videoID, err := uuid.Parse(videoIDString)
 	if err != nil {
@@ -28,10 +50,31 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Видео недоступно", err)
+		return
+	}
+
+	thumb := thumbnail{
+		data:      byteImage,
+		mediaType: contentType,
+	}
+
+	videoThumbnails[videoID] = thumb
+	scheme := "http"
+	if r.TLS != nil {
+		scheme = "https"
+	}
+	urlThumb := fmt.Sprintf("%s://%s/api/thumbnails/%s", scheme, r.Host, videoIDString)
+	video.ThumbnailURL = &urlThumb
+
+	if err := cfg.db.UpdateVideo(video); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to write database", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
-
-	// TODO: implement the upload here
-
-	respondWithJSON(w, http.StatusOK, struct{}{})
 }
