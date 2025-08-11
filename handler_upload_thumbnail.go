a3659bf2f9021233e6eeaf9a93a94668643830a3
Sergleq <ma4mal@gmail.com>
2025-08-11 01:38:29 +0000

    upload hundler refactoring

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,39 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"mime"
 	"net/http"
 	"os"
-	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
-	maxMemory := 10 << 20
-	if err := r.ParseMultipartForm(int64(maxMemory)); err != nil {
-		respondWithError(w, http.StatusInternalServerError, "parsing is fail", err)
-		return
-	}
-
-	file, header, err := r.FormFile("thumbnail")
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
-		return
-	}
-	defer file.Close()
-
-	contentType := header.Header.Get("Content-Type")
-
-	// byteImage, err := io.ReadAll(file)
-	// if err != nil {
-	// 	respondWithError(w, http.StatusInternalServerError, "Unable to read form file", err)
-	// 	return
-	// }
-
 	videoIDString := r.PathValue("videoID")
 	videoID, err := uuid.Parse(videoIDString)
 	if err != nil {
@@ -53,50 +30,57 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	const maxMemory = 10 << 20 // 10 MB
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Content-Type", err)
+		return
+	}
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Invalid file type", nil)
+		return
+	}
+
+	assetPath := getAssetPath(videoID, mediaType)
+	assetDiskPath := cfg.getAssetDiskPath(assetPath)
+
+	dst, err := os.Create(assetDiskPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to create file on server", err)
+		return
+	}
+	defer dst.Close()
+	if _, err = io.Copy(dst, file); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error saving file", err)
+		return
+	}
+
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
-		respondWithError(w, http.StatusUnauthorized, "Видео недоступно", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't find video", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not authorized to update this video", nil)
 		return
 	}
 
-	// thumb := thumbnail{
-	// 	data:      byteImage,
-	// 	mediaType: contentType,
-	// }
-	// videoThumbnails[videoID] = thumb
-	// stringImage := base64.StdEncoding.EncodeToString(byteImage)
-
-	ext, _ := mime.ExtensionsByType(contentType)
-	if len(ext) == 0 {
-		ext = []string{".bin"}
-	}
-	videoTypeStr := fmt.Sprintf("%s%s", videoIDString, ext[0])
-	filePath := filepath.Join(cfg.assetsRoot, videoTypeStr)
-
-	fileCreate, err := os.Create(filePath)
+	url := cfg.getAssetURL(assetPath)
+	video.ThumbnailURL = &url
+	err = cfg.db.UpdateVideo(video)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "не удалось создать файл", err)
-		return
-	}
-	if _, err := io.Copy(fileCreate, file); err != nil {
-		respondWithError(w, http.StatusInternalServerError, "не удалось копировать файл", err)
-		return
-	}
-
-	scheme := "http"
-	if r.TLS != nil {
-		scheme = "https"
-	}
-	urlThumb := fmt.Sprintf("%s://%s/assets/%s", scheme, r.Host, videoTypeStr)
-	// urlThumb := fmt.Sprintf("data:%s;base64,%s", contentType, stringImage)
-	video.ThumbnailURL = &urlThumb
-
-	if err := cfg.db.UpdateVideo(video); err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Unable to write database", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
 		return
 	}
 
 	respondWithJSON(w, http.StatusOK, video)
-
-	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 }
